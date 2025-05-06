90e299172132d4a731bc185fc3611163081f2552
Cameron McCellan <mcc18023@byui.edu>
2025-05-06 13:33:14 -0600

    creation of theme_lc function and general modifications

---
+++ R/theme_lc.R
@@ -0,0 +1,15 @@
+#' @export
+theme_lc <- function(){
+
+  theme_minimal() %+replace%
+
+  theme(
+    plot.title = element_text(color = "#B07E63"),
+    axis.title.y = element_text(color = "#D3B593"),
+    axis.title.x = element_text(color = "#D3B593"),
+    panel.grid.major = element_line(color = "#D3B593", linewidth = .06),
+    axis.text.x = element_text(color = "#78776C"),
+    axis.text.y = element_text(color = "#78776C"),
+    panel.grid.minor = element_blank()
+  )
+}
