c60e285b4a411df191a9907b2a9be3bb4390d70c
Cameron McCellan <mcc18023@byui.edu>
2025-05-22 16:47:05 -0600

    slice progress

--- R/geom_slice.R
+++ R/geom_slice.R
@@ -1,9 +1,9 @@
-model <- lm(width ~ length, KidsFeet)
+model <- lm(width ~ length + sex, KidsFeet)
 
-geom_slice <- function(model,xaxis=NA,n=100, ...){
+geom_slice <- function(model,xaxis=NA,n=100, color = 'skyblue',linewidth=2, linetype = 'solid', alpha = 1,...){
 
   if (!inherits(model, 'lm'))
-    stop(message = 'Model must be in lm format')
+    stop('Model must be in lm format')
 
   if (is.na(xaxis)){
     xaxis <- names(model$model)[2]
@@ -17,44 +17,56 @@ geom_slice <- function(model,xaxis=NA,n=100, ...){
 
   other_vars <- setdiff(names(model$model), c(y_name, xaxis))
 
-  dots <- list(...)
+  if(length(other_vars > 0)) {
 
-  means_list <- lapply(other_vars, function(var) {
-    if (!is.null(dots[[var]])) {
-      dots[[var]]
-    } else {
-      col <- model$model[[var]]
-      if (is.numeric(col)) {
-        mean(col, na.rm = TRUE)
-      } else if (is.factor(col)) {
-        levels(col)[1]
+    dots <- list(...)
+
+    means_list <- lapply(other_vars, function(var) {
+      if (!is.null(dots[[var]])) {
+        dots[[var]]
       } else {
-        stop(paste("Unsupported variable type for:", var))
+        col <- model$model[[var]]
+        if (is.numeric(col)) {
+          mean(col, na.rm = TRUE)
+        } else if (is.factor(col)) {
+          levels(col)[1]
+        } else {
+          stop(paste("Unsupported variable type for:", var))
+        }
       }
-    }
-  })
+    })
 
-  x_names <- as.data.frame(as.list(setNames(means_list,other_vars)))
+    x_names <- as.data.frame(as.list(setNames(means_list,other_vars)))
 
-  vals <- seq(min(x1), max(x1), length.out = n)
+    vals <- seq(min(x1), max(x1), length.out = n)
 
-  new_data <- setNames(data.frame(vals),xaxis)
+    new_data <- setNames(data.frame(vals),xaxis)
 
-  new_data <- cbind(new_data, x_names)
+    new_data <- cbind(new_data, x_names)
+
+  }else{
+    message('Model is a simple linear regression')
+
+    vals <- seq(min(x1), max(x1), length.out = n)
+
+    new_data <- setNames(data.frame(vals),xaxis)
+  }
 
   new_data$preds <- predict(model, newdata = new_data)
 
-  geom_line(
-    data=new_data,
-    mapping = aes(x=xaxis,y=preds),
-    color=color,
-    linewidth=1,
-    inherit.aes = FALSE
-  )
+  eval(parse(text=paste0('geom_line(',
+    'data=new_data,',
+    'mapping = aes(x=',xaxis,',y=preds),',
+    'color = color,',
+    'linewidth= linewidth,',
+    'alpha = alpha,',
+    'linetype = linetype,',
+    'inherit.aes = FALSE)'
+  )))
 
 }
 
 ggplot(KidsFeet, aes(x=length,
                      y=width))+
   geom_point()+
-  geom_slice(model, xaxis='length')
+  geom_slice(model, xaxis='length', sex='B')
--- R/slice_2d.R
+++ R/slice_2d.R
@@ -1,13 +1,13 @@
-model <- lm(width ~ length + sex, KidsFeet)
+model <- lm(width ~ length + birthmonth + sex, KidsFeet)
 
 slice_2d <- function(model,xaxis=NA,n=100, ...){
 
   if (!inherits(model, 'lm'))
-    stop(message = 'Model must be in lm format')
+    stop('Model must be in lm format')
 
   if (is.na(xaxis)){
     xaxis <- names(model$model)[2]
-    warning('X-axis not specified; used first x variable as x-axis')
+    warning('X-axis not specified; used first x variable as x-axis: Use xaxis=\"var_name\"')
   }
 
   y <- model$model[1][[1]]
@@ -15,18 +15,25 @@ slice_2d <- function(model,xaxis=NA,n=100, ...){
 
   y_name <- names(model$model)[1]
 
+  if (!(xaxis %in% names(model$model))) {
+    stop(paste("xaxis variable", xaxis, "not found in the model."))
+  }
+
   other_vars <- setdiff(names(model$model), c(y_name, xaxis))
 
   dots <- list(...)
 
   means_list <- lapply(other_vars, function(var) {
     if (!is.null(dots[[var]])) {
+      message('Slice variable not specified:\n Use \"var_name\" = value to specify')
       dots[[var]]
     } else {
       col <- model$model[[var]]
       if (is.numeric(col)) {
+        message('Slice value not specified - Used mean of ', var)
         mean(col, na.rm = TRUE)
       } else if (is.factor(col)) {
+        message('Slice value not specified - Used first level of ', var)
         levels(col)[1]
       } else {
         stop(paste("Unsupported variable type for:", var))
@@ -44,12 +51,72 @@ slice_2d <- function(model,xaxis=NA,n=100, ...){
 
   new_data$preds <- predict(model, newdata = new_data)
 
-  plot(new_data[[xaxis]], new_data$preds,
-       type = "l",
+  plot(y ~ x1,
        xlab = xaxis,
-       ylab = paste("Predicted", y_name),
-       ...)
+       ylab = paste("Predicted", y_name))
+
+  lines(new_data[[xaxis]],new_data$preds)
 
 }
 
-slice_2d(model,xaxis = 'length', sex='B')
+slice_2d(model)
+
+add_slice_2d <- function(model,xaxis=NA,n=100, ...){
+
+  if (!inherits(model, 'lm'))
+    stop('Model must be in lm format')
+
+  if (is.na(xaxis)){
+    xaxis <- names(model$model)[2]
+    message('X-axis not specified; used first x variable as x-axis: Use xaxis=\"var_name\"')
+  }
+
+  y <- model$model[1][[1]]
+  x1 <- model$model[[xaxis]]
+
+  y_name <- names(model$model)[1]
+
+  if (!(xaxis %in% names(model$model))) {
+    stop(paste("xaxis variable", xaxis, "not found in the model."))
+  }
+
+  other_vars <- setdiff(names(model$model), c(y_name, xaxis))
+
+  dots <- list(...)
+
+  means_list <- lapply(other_vars, function(var) {
+    if (!is.null(dots[[var]])) {
+      message('Slice variable not specified:\n Use \"var_name\" = value to specify')
+      dots[[var]]
+    } else {
+      col <- model$model[[var]]
+      if (is.numeric(col)) {
+        message('Slice value not specified - Used mean of ', var)
+        mean(col, na.rm = TRUE)
+      } else if (is.factor(col)) {
+        message('Slice value not specified - Used first level of ', var)
+        levels(col)[1]
+      } else {
+        stop(paste("Unsupported variable type for:", var))
+      }
+    }
+  })
+
+  x_names <- as.data.frame(as.list(setNames(means_list,other_vars)))
+
+  vals <- seq(min(x1), max(x1), length.out = n)
+
+  new_data <- setNames(data.frame(vals),xaxis)
+
+  new_data <- cbind(new_data, x_names)
+
+  new_data$preds <- predict(model, newdata = new_data)
+
+  lines(new_data[[xaxis]],new_data$preds)
+
+}
+
+
+kids <- lm(width ~ length + birthmonth, data = KidsFeet)
+plot(width ~ length, data = KidsFeet)
+add_slice_2d(kids)
