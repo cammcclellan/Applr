780512b5459ad35e9d184827f6f9dda18ac89fab
Cameron McCellan <mcc18023@byui.edu>
2025-06-03 14:46:22 -0600

    more work and research on ggproto and trying to make the function work

--- R/geom_slice.R
+++ R/geom_slice.R
@@ -1,73 +1,6 @@
 
 library(mosaic)
-model <- lm(totalbill ~ temp + ccf + temp:ccf + month, Utilities)
-
-
-geom_slice <- function(model,xaxis=NA,n=100, color = 'skyblue',linewidth=2, linetype = 'solid', alpha = 1,...){
-
-  if (!inherits(model, 'lm'))
-    stop('Model must be in lm format')
-
-  if (is.na(xaxis)){
-    xaxis <- names(model$model)[2]
-    warning('X-axis not specified; used first x variable as x-axis')
-  }
-
-  y <- model$model[1][[1]]
-  x1 <- model$model[[xaxis]]
-
-  y_name <- names(model$model)[1]
-
-  other_vars <- setdiff(names(model$model), c(y_name, xaxis))
-
-  if(length(other_vars) > 0) {
-
-    dots <- list(...)
-
-    means_list <- lapply(other_vars, function(var) {
-      if (!is.null(dots[[var]])) {
-        dots[[var]]
-      } else {
-        col <- model$model[[var]]
-        if (is.numeric(col)) {
-          mean(col, na.rm = TRUE)
-        } else if (is.factor(col)) {
-          levels(col)[1]
-        } else {
-          stop(paste("Unsupported variable type for:", var))
-        }
-      }
-    })
-
-    x_names <- as.data.frame(as.list(setNames(means_list,other_vars)))
-
-    vals <- seq(min(x1), max(x1), length.out = n)
-
-    new_data <- setNames(data.frame(vals),xaxis)
-
-    new_data <- cbind(new_data, x_names)
-
-  }else{
-    message('Model is a simple linear regression')
-
-    vals <- seq(min(x1), max(x1), length.out = n)
-
-    new_data <- setNames(data.frame(vals),xaxis)
-  }
-
-  new_data$preds <- predict(model, newdata = new_data)
-
-  eval(parse(text=paste0('geom_line(',
-                         'data=new_data,',
-                         'mapping = aes(x=',xaxis,',y=preds),',
-                         'color = color,',
-                         'linewidth= linewidth,',
-                         'alpha = alpha,',
-                         'linetype = linetype,',
-                         'inherit.aes = FALSE)'
-  )))
-
-}
+model <- lm(totalbill ~ temp + month, Utilities)
 
 StatSlice <- ggproto("StatSlice", Stat,
                      compute_group = function(data, scales, model, xaxis = NULL, n = 100, ...) {
@@ -83,20 +16,18 @@ StatSlice <- ggproto("StatSlice", Stat,
 
                        dots <- list(...)
 
-                       means_list <- lapply(other_vars, function(var) {
-                         if (!is.null(dots[[var]])) {
-                           dots[[var]]
-                         } else {
-                           col <- model$model[[var]]
-                           if (is.numeric(col)) {
-                             mean(col, na.rm = TRUE)
-                           } else if (is.factor(col)) {
-                             levels(col)[1]
-                           } else {
-                             stop(paste("Unsupported variable type for:", var))
-                           }
-                         }
-                       })
+                       # means_list <- lapply(other_vars, function(var) {
+                       #   if (var %in% names(data)) {
+                       #     unique_val <- unique(data[[var]])
+                       #     if (length(unique_val) > 1){
+                       #       warning(paste("Multiple values for", var, "in facet group"))}
+                       #     unique_val[1]
+                       #   } else {
+                       #     col <- model$model[[var]]
+                       #   }
+                       # })
+
+                       means_list <-
 
                        x_names <- as.data.frame(as.list(setNames(means_list, other_vars)))
                        vals <- seq(min(x1, na.rm = TRUE), max(x1, na.rm = TRUE), length.out = n)
@@ -116,7 +47,7 @@ GeomSlice <- ggproto('GeomSlice',GeomLine,
                      default_aes = aes(color = "skyblue", linewidth = 1, linetype = "solid", alpha = 1)
                      )
 
-# Wrapper function for the geom
+
 geom_slice <- function(model, xaxis = NULL, n = 100,inherit.aes = TRUE, ...){
   layer(
     stat = StatSlice,
@@ -135,7 +66,9 @@ ggplot(Utilities, aes(x=temp,
   geom_point()+
   facet_wrap(~month)+
   geom_slice(model, xaxis='temp')+
-  stat_function(fun = function(x) b[1]+b[2]*x+b[3]*1)
+  stat_function(fun = function(x) b[1]+b[2]*x+b[3]*12)+
+  geom_smooth(method='lm',formula=y~x,se=F)
+
 
 
 
